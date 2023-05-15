968334f521266ef4502f9e9e017ed05f34788714
TimaDootaliev <timur.dootaliev.td@gmail.com>
2023-05-15 12:22:39 +0600

    intro

---
+++ intro/product/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=200)
+    description = serializers.CharField()
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    in_stock = serializers.BooleanField()
--- intro/product/views.py
+++ intro/product/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+@api_view(['GET'])
+def get_products(request: Request):
+    queryset = Product.objects.all()
+    # SELECT * FROM product;
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+
+# TODO: разверните 3 проекта и залейте на 3 разных репозитория на гитхаб
